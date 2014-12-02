84c22a607d2481268160f1dfa54d18e4d7730d1c
toptan53 <emre_toptan@web.de>
2014-12-02 16:04:20 +0100

    tetssasas

---
+++ src/wsn/zigbee/WSNZigBee.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wsn.zigbee;
+import javax.swing.*;
+
+/**
+ *
+ * @author user
+ */
+public class WSNZigBee extends JFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
